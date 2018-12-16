9965173d0fff4f3f9ad70f67710a8afca1a6f1d4
sieuwe elferink <sieuwe_crossen@hotmail.com>
2018-12-16 21:36:22 +0100

    added ComPin support and fixed accidental triggers

--- AutomaticDoor.ino
+++ AutomaticDoor.ino
@@ -1,11 +1,11 @@
-//By Sieuwe Elferink
+/By Sieuwe Elferink
 
 //config
 ////////////////
-const int MotorDelay = 2800; //change this value to make the door open more or less
+const int MotorDelay = 3100; //change this value to make the door open more or less
 const int TimeDelay = 4000; // change this value to change the time between the door opening and closing
 const int ActivationDistance = 50; //change this value to change the distance from your hand to the sensor to activate the door.
- ///////////////
+///////////////
 
 const int trigPin = 4;
 const int echoPin = 3;
@@ -13,12 +13,14 @@ const int echoPin = 3;
 const int trigPin2 = 10;
 const int echoPin2 = 11;
 
+const int ComPin = 5; //Pin for communicating with the CortanaRoom application. Check the CortanaRoom reposetory for more info!
+
 long duration, duration2;
 int distanceCm, distanceCm2;
 
 
 void setup() {
- 
+
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   digitalWrite(8, LOW);
@@ -29,6 +31,11 @@ void setup() {
 
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
+
+  pinMode(ComPin, OUTPUT);
+
+  Serial.begin(9600);
+
 }
 
 
@@ -51,17 +58,12 @@ void loop() {
   duration2 = pulseIn(echoPin2, HIGH);
   distanceCm2 = duration2 * 0.034 / 2;
 
-  if (distanceCm <= ActivationDistance) {
-    digitalWrite(8, HIGH);
-    delay(MotorDelay);
-    digitalWrite(8, LOW);
-    delay(TimeDelay);
-    digitalWrite(9, HIGH);
-    delay(MotorDelay);
-    digitalWrite(9, LOW);;
-  }
+  Serial.print(distanceCm);
+  Serial.print("    ");
+  Serial.println(distanceCm2);
 
-  if (distanceCm2 <= ActivationDistance) {
+  if (distanceCm <= ActivationDistance) {
+    digitalWrite(ComPin, HIGH);
     digitalWrite(8, HIGH);
     delay(MotorDelay);
     digitalWrite(8, LOW);
@@ -69,12 +71,20 @@ void loop() {
     digitalWrite(9, HIGH);
     delay(MotorDelay);
     digitalWrite(9, LOW);
+    delay(2000);
+    digitalWrite(ComPin, LOW);
+  }
+
+  if (distanceCm2 <= ActivationDistance) {
+    digitalWrite(ComPin, HIGH);
+    digitalWrite(8, HIGH);
+    delay(MotorDelay);
+    digitalWrite(8, LOW);
+    delay(TimeDelay);
+    digitalWrite(9, HIGH);
+    delay(MotorDelay);
+    digitalWrite(9, LOW);
+    delay(2000);
+    digitalWrite(ComPin, LOW);
   }
 }
-
-
-
-
-
-
-
